039faf27f491f55a66832744e2384447363eec97
Sofia Vei <92432705+SophiaVei@users.noreply.github.com>
2023-06-07 17:52:35 +0300

    Update Sofia-Vei-140-Ass2.sol

--- Sofia-Vei-140-Ass2.sol
+++ Sofia-Vei-140-Ass2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract CharityDonation {
     // Array to store the addresses of charities
-    address[] public charities;
+    address[] public charities; // Although the charities array is publicly accessible, it only provides the length of the array and not the individual addresses of the charities.
 
     // Address of the contract owner
     address public owner;
@@ -25,13 +25,14 @@ contract CharityDonation {
 
     // Constructor that accepts an array of charity addresses at contract deployment
     constructor(address[] memory _charities) {
+        require(_charities.length > 0, "Charities array must not be empty");
         charities = _charities;
         owner = msg.sender;
     }
 
     // Function for making a donation to a specified charity and destination address
     // Without the donation amount parameter - serves as a simplified version of the donation method
-    // Basic implementation of the donation functionality. Check later on in the code for overloading method
+    // This is the basic implementation of the donation functionality. Check later on in the code for overloading method
     function donate(address destination, uint256 charityIndex) public payable {
         // Check if the charity index is valid
         require(charityIndex < charities.length, "Invalid charity index");
@@ -54,21 +55,23 @@ contract CharityDonation {
         totalDonations += donationAmount;
 
         // Check if the current donation is the highest so far
-        if (msg.value > highestDonation) {
-            highestDonation = msg.value;
+        if (donationAmount > highestDonation) {
+            highestDonation = donationAmount;
             highestDonor = msg.sender;
         }
 
         // Emit an event to indicate the donation
-        emit Donation(msg.sender, msg.value);
+        emit Donation(msg.sender, donationAmount);
     }
 
     // Overloaded version of the donate method that accepts a donation amount
-    // This function implements method overloadingon to a specified charity and destination address with a specific donation amount
+    // This function implements method overloading on a specified charity and destination address with a specific donation amount
     function donate(address destination, uint256 charityIndex, uint256 donationAmount) public payable {
         // Check if the donation amount is within acceptable limits
-        require(donationAmount >= totalDonations / 100, "Donation amount is below minimum");
-        require(donationAmount <= totalDonations / 2, "Donation amount exceeds maximum");
+        uint256 minimumDonation = totalDonations / 100;
+        uint256 maximumDonation = totalDonations / 2;
+        require(donationAmount >= minimumDonation, "Donation amount is below minimum");
+        require(donationAmount <= maximumDonation, "Donation amount exceeds maximum");
 
         // Check if the charity index is valid
         require(charityIndex < charities.length, "Invalid charity index");
@@ -80,47 +83,42 @@ contract CharityDonation {
         uint256 charityAmount = donationAmount / 10;
         uint256 destinationAmount = donationAmount - charityAmount;
 
-        // Transfer the charity amount to the specified charity
-        payable(charities[charityIndex]).transfer(charityAmount);
+            // Transfer the charity amount to the specified charity
+    payable(charities[charityIndex]).transfer(charityAmount);
 
-        // Transfer the destination amount to the specified destination address
-        payable(destination).transfer(destinationAmount);
+    // Transfer the destination amount to the specified destination address
+    payable(destination).transfer(destinationAmount);
 
-        // Update the total donations
-        totalDonations += donationAmount;
+    // Update the total donations
+    totalDonations += donationAmount;
 
-        // Check if the current donation is the highest so far
-        if (msg.value > highestDonation) {
-            highestDonation = msg.value;
-            highestDonor = msg.sender;
-        }
-
-        // Emit an event to indicate the donation
-        emit Donation(msg.sender, msg.value);
+    // Check if the current donation is the highest so far
+    if (donationAmount > highestDonation) {
+        highestDonation = donationAmount;
+        highestDonor = msg.sender;
     }
 
-    // Function to retrieve the total amount of donations received
-        function getTotalDonations() public view returns (uint256) {
-        return totalDonations;
-    }
-
-    // Function to retrieve the address and amount of the highest donor
-    function getHighestDonor() public view onlyOwner returns (address, uint256) {
-        return (highestDonor, highestDonation);
-    }
-
-    // Function to destroy the contract and transfer its balance to the contract owner
-    function destroy() public onlyOwner {
-        payable(owner).transfer(address(this).balance);
-    }
-
-    // Event to emit when a donation is made
-    event Donation(address indexed donor, uint256 amount);
+    // Emit an event to indicate the donation
+    emit Donation(msg.sender, donationAmount);
 }
 
-// Note: the use of selfdestruct was only avoided because solidity by default raises a warning for it, which is also mentioned in the documentation https://docs.soliditylang.org/en/v0.8.20/solidity-by-example.html
-// However, just to have an insight, it would be used like this: 
-// //Function to destroy the contract and transfer its balance to the contract owner
-// function destroy() public onlyOwner {
-//     selfdestruct(payable(owner));
-//}
+// Function to retrieve the total amount of donations received
+function getTotalDonations() public view returns (uint256) {
+    return totalDonations;
+}
+
+// Function to retrieve the address and amount of the highest donor
+function getHighestDonor() public view onlyOwner returns (address, uint256) {
+    return (highestDonor, highestDonation);
+}
+
+// Note: In the use of selfdestruct Solidity by default raises a warning, which is also mentioned in the documentation https://docs.soliditylang.org/en/v0.8.20/solidity-by-example.html
+// Function to destroy the contract and transfer its balance to the contract owner
+function destroy() public onlyOwner {
+    selfdestruct(payable(owner));
+}
+
+// Event to emit when a donation is made
+event Donation(address indexed donor, uint256 amount);
+}
+
